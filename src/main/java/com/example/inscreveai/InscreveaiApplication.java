615434eab0a049dc248f5bf19e03df4a3706c945
Lucas Irvin <lucasgamerba@gmail.com>
2025-01-06 00:52:41 -0300

    att

---
+++ src/main/java/com/example/inscreveai/InscreveaiApplication.java
@@ -0,0 +1,13 @@
+package com.example.inscreveai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InscreveaiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InscreveaiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/inscreveai/InscreveaiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.inscreveai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InscreveaiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
